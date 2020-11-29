880609020adf7f8a13facfe40a55a3c78a80a7c7
Raufeder <you@example.com>
2020-11-29 12:38:55 -0500

    todo11/28

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const { Router } = require('express');
 const { User } = require("../models");
 
+//TODO Admin Routes (Create Routes, Delete Routes, Get Route List, Update Password )
 const adminController = Router();
 
 module.exports = adminController;
--- controllers/routeController.js
+++ controllers/routeController.js
@@ -2,5 +2,6 @@ const { Router } = require("express");
 const { Route } = require("../models");
 
 const routeController = Router();
+//TODO Route Endpoints (Check Planning Sheet)
 
 module.exports = routeController;
