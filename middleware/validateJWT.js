156add2bc6d817d1d81032adeb5a0bd43b8c771e
Raufeder <you@example.com>
2020-12-11 06:42:18 -0500

    fixedJWT

--- middleware/validateJWT.js
+++ middleware/validateJWT.js
@@ -5,29 +5,26 @@ const ValidateJWTMiddleware = (req, res, next) => {
   if (req.method === 'OPTIONS') {
     return next();
   } else if (req.headers.authorization) {
-    const token = req.headers.authorization;
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-
-      
-      if (token) {
-        User.findOne({
-          where: {
-            id: decoded.id
-          }
-        })
-        .then(user => {
-          req.user = user;
-          next();
-        })
-      } else {
-        res.status(401).json({
-          message: 'Not allowed'
-        });
-      }
-    });
+    const { authorization } = req.headers;
+    const payload = jwt.verify(authorization, process.env.JWT_SECRET);
+    if (payload) {
+      User.findOne({
+        where: {
+          id: payload.id
+        }
+      })
+      .then(user => {
+        req.user = user;
+        next();
+      })
+    } else {
+      res.status(401).json({
+        message: 'Not allowed'
+      });
+    }
   } else {
     res.status(401).json({
-      message: 'whoops'
+      message: 'Whoops'
     });
   }
 }
